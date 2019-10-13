6745a81f7583f8f6d6d2db03210bd4488ecddb23
Enrico Gaffo <enrico.gaffo@gmail.com>
2019-10-13 12:05:54 +0200

    added sconscript to run Cutadapt

---
+++ src/sconstructs/cutadapt.py
@@ -0,0 +1,46 @@
+Import('*')
+
+try:
+    # these are the variables passed with 'exports' from a calling SConscript
+    env = env_cutadapt.Clone()
+except NameError:
+    vars = Variables('vars.py')
+    vars.Add('ADAPTER', '', '')
+    vars.Add('QUALITY_ENCODING', '33 for Phread score, 64 for old Solexa', '')
+    vars.Add('SAMPLE', 'just a name to prefix to files', '')
+    vars.Add('MIN_LEN', 
+             '', 
+             '15')
+    vars.Add('MAX_LEN', 
+             'Max read length. Reads longer than MAX_LEN after adapter trim '\
+             'are saved in a separate file', 
+             '34')
+    vars.Add('ADPT_OVERLAP', 'Min lenght of adapter overlap', '')
+    vars.Add('CPUS', 
+             '', 
+             '1')
+    vars.Add('CUTADAPT_EXTRA_PARAMS', '', '')
+    
+    env = Environment(ENV=os.environ,
+                      variables=vars)
+    
+    Help(vars.GenerateHelpText(env))
+    unknown = vars.UnknownVariables()
+    if unknown:
+        print "Unknown variables:", unknown.keys()
+        Exit(1)
+
+env.SetDefault(ADPT_OVERLAP = 5)
+
+cutadapt_cmd = '''cutadapt $( -j $CPUS $) -a $ADAPTER --no-indels -n 10 -O $ADPT_OVERLAP '''\
+               '''--quality-base $QUALITY_ENCODING -m $MIN_LEN -M MAX_LEN --trimmed-only '''\
+               '''--too-long-output ${TARGETS[1]} $CUTADAPT_EXTRA_PARAMS -o ${TARGETS[0]} '''\
+               '''${SOURCES[0]} > ${TARGETS[2]}'''
+
+cutadapt =  env.Command(['${SAMPLE}_trimmed_reads.fq.gz', 
+                         '${SAMPLE}_long_reads.fq.gz',
+                         '${SAMPLE}_cutadapt.log'], 
+                        env['READS'], 
+                        cutadapt_cmd)
+
+Return('cutadapt')
