f820c82efecfbc7719ef8ee541a7d81d74e1d12d
Enrico Gaffo <enrico.gaffo@gmail.com>
2019-10-25 23:38:34 +0200

    added parameter to set number of parallel tasks to run

--- src/sconstructs/filter.py
+++ src/sconstructs/filter.py
@@ -39,7 +39,7 @@ if not env['LONG_READS_FILE'] == '':
 filtered_cmd = filtered_cmd + \
                '''-l $MAX_LEN_FILTER -q $MEAN_QUAL_FILTER '''\
                '''-e $QUALITY_ENCODING -r ${TARGETS[1]} -p 20 '''\
-               '''-o ${TARGETS[0]}'''
+               '''-o ${TARGETS[0]} $(-j $CPUS $) '''
 
 filtered = env.Command(targets, 
                        env['READS'], 
