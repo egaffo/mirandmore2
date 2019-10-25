77f56b28da625ba7bf10547b435d5a1d69bb2cf9
Enrico Gaffo <enrico.gaffo@gmail.com>
2019-10-25 20:21:03 +0200

    parallelized filter_trimmed.py using Pool and two listening queues to write output

--- src/utils/filter_trimmed.py
+++ src/utils/filter_trimmed.py
@@ -7,37 +7,109 @@ Filter FASTQ reads according to quality and length
 from __future__ import print_function
 import argparse, gzip, HTSeq, sys
 import multiprocessing as mp
+from multiprocessing import Pool, Value
 
+### https://gist.github.com/ngcrawford/2237170
+#from itertools import izip_longest
+#
+#def process_chunk(d):
+#    """Replace this with your own function
+#    that processes data one line at a
+#    time"""
+#
+#    d = d.strip() + ' processed'
+#    return d 
+#
+#def grouper(n, iterable, padvalue=None):
+#    """grouper(3, 'abcdefg', 'x') -->
+#    ('a','b','c'), ('d','e','f'), ('g','x','x')"""
+#
+#    return izip_longest(*[iter(iterable)]*n, fillvalue=padvalue)
+#
+#def main():
+#p = multiprocessing.Pool(4)
+#
+#    # Use 'grouper' to split test data into
+#    # groups you can process without using a
+#    # ton of RAM. You'll probably want to 
+#    # increase the chunk size considerably
+#    # to something like 1000 lines per core.
+#
+#    # The idea is that you replace 'test_data'
+#    # with a file-handle
+#    # e.g., testdata = open(file.txt,'rU')
+#
+#    # And, you'd write to a file instead of
+#    # printing to the stout
+#
+#    for chunk in grouper(10, test_data):
+#       results = p.map(process_chunk, chunk)
+#      for r in results:
+#                  print r
+#                  # replace
+#                  # with
+#                  # outfile.write()
 
-def filter_n_split_read(read, min_base_qual, mean_quality, 
-                        max_length, outfile, long_read_file):
+def mirna_reads_writer(q):
+    ## assume a outfile file handler has been instantiated
+    while 1:
+        read = q.get()
+        if str(read) == 'kill':
+            break
+        outfile.write('\n'.join(read) + '\n')
+        outfile.flush()
+
+def long_reads_writer(q):
+    ## assume a long_read_file file handler has been instantiated
+    while 1:
+        read = q.get()
+        if str(read) == 'kill':
+            break
+        if long_read_file:
+            long_read_file.write('\n'.join(read) + '\n')
+            long_read_file.flush()
+
+def filter_n_split_read(read, min_base_qual, mean_quality, max_length, 
+                        outfile_queue, long_read_file_queue):
     
     global processed_read_count
     global low_mean_base_qual_read
     global long_read_count
     global low_base_qual_read_count
 
-    #with processed_read_count.get_lock()
-    processed_read_count += 1
-    read_len = len(read.qual)
-    read_mean_qual = sum(read.qual)/float(read_len)
-    read_lowqual_bases = sum(read.qual < min_base_qual)
-    
+    with processed_read_count.get_lock():
+        processed_read_count.value += 1
+
+    htread = HTSeq.SequenceWithQualities(read[0].encode(), 
+                                         read[1],
+                                         read[2].encode())
+
+    read_len = len(htread.qual)
+    read_mean_qual = sum(htread.qual)/float(read_len)
+    read_lowqual_bases = sum(htread.qual < min_base_qual)
+
+    ## format FASTQ entry to print
+    read = ('@' + read[1], read[0], '+', read[2])
+   
     if read_lowqual_bases < 2:
         if read_mean_qual >= mean_quality:
             if read_len <= max_length:
-                read.write_to_fastq_file(outfile)
+                ## save to reads-passing-filters file
+                outfile_queue.put(read)
 
             else:
-                long_read_count += 1
-                if not args.long_read_file == '':
-                    read.write_to_fastq_file(long_read_file)
+                with long_read_count.get_lock():
+                    long_read_count.value += 1
+                ## save to lenght-discarded read file
+                long_read_file_queue.put(read)
 
         else:
-            low_mean_base_qual_read += 1
+            with low_mean_base_qual_read.get_lock():
+                low_mean_base_qual_read.value += 1
 
     else:
-        low_base_qual_read_count += 1
+        with low_base_qual_read_count.get_lock():
+            low_base_qual_read_count.value += 1
 
     return None
 
@@ -85,6 +157,13 @@ if __name__ == '__main__':
                         required = False,
                         type = int)
 
+    parser.add_argument('-j', '--jobs',
+                        dest = 'jobs', 
+                        default = 1,
+                        required = False,
+                        type = int,
+                        help = 'The number of parallel jobs to execute')
+
     args = parser.parse_args()
 
     ## handle compressed files 
@@ -97,7 +176,9 @@ if __name__ == '__main__':
     else:
         f = open(args.fastq_file, 'r')
 
-    fastq_file = HTSeq.FastqReader(f, args.quality_encoding)
+    fastq_file = HTSeq.FastqReader(f, 
+                                   args.quality_encoding,
+                                   raw_iterator = True)
 
     if args.output == '-':
         outfile = sys.stdout
@@ -107,8 +188,8 @@ if __name__ == '__main__':
     else:
         outfile = open(args.output, 'w')
 
-    if not args.long_read_file == '':
-        
+    long_read_file = None
+    if not args.long_read_file == '':        
         ## get file handler to save discarded reads from length filter
         if args.long_read_file.lower().endswith(".gz"):
             long_read_file = gzip.open(args.long_read_file, 'wt')
@@ -117,18 +198,35 @@ if __name__ == '__main__':
             long_read_file = open(args.long_read_file, 'w')
 
     ## initialize global variables
-    low_base_qual_read_count = 0
-    low_mean_base_qual_read  = 0
-    long_read_count = 0
-    processed_read_count = 0
+    low_base_qual_read_count = Value('i', 0)
+    low_mean_base_qual_read  = Value('i', 0)
+    long_read_count = Value('i', 0)
+    processed_read_count = Value('i', 0)
 
-    for read in fastq_file:
-        filter_n_split_read(read, 
-                            args.min_base_qual, 
-                            args.mean_quality, 
-                            args.max_length, 
-                            outfile, 
-                            long_read_file)
+    manager = mp.Manager()
+    
+    pool = mp.Pool(processes = args.jobs)
+
+    outfile_queue = manager.Queue()
+    watcher_1 = pool.apply_async(mirna_reads_writer, (outfile_queue,))
+
+    long_read_file_queue = manager.Queue()
+    watcher_2 = pool.apply_async(long_reads_writer, (long_read_file_queue,))
+
+    #res = pool.starmap(filter_n_split_read, 
+    res = pool.starmap_async(filter_n_split_read, 
+                             [(read,
+                               args.min_base_qual, 
+                               args.mean_quality, 
+                               args.max_length,
+                               outfile_queue,
+                               long_read_file_queue) for read in fastq_file], 
+                             chunksize = 10000)
+    
+    outfile_queue.put('kill')
+    long_read_file_queue.put('kill')
+    pool.close()
+    pool.join()
 
     if not args.long_read_file == '':
         long_read_file.close()
@@ -136,22 +234,22 @@ if __name__ == '__main__':
     outfile.close()
 
     with open(args.report_file, "w") as report_file:
-        print('''{} processed reads'''.format(processed_read_count), 
+        print('''{} processed reads'''.format(processed_read_count.value), 
               file = report_file)
 
         print('''{} reads discarded because > '''\
               '''{} bases had quality < '''\
-              '''{}'''.format(low_base_qual_read_count,
+              '''{}'''.format(low_base_qual_read_count.value,
                               2, 
                               args.min_base_qual), 
               file = report_file)
 
         print('''{} reads discarded because mean quality '''\
-              '''was < {}'''.format(low_mean_base_qual_read, 
+              '''was < {}'''.format(low_mean_base_qual_read.value, 
                                     args.mean_quality), 
               file = report_file)
 
-        print('''{} reads were > {} nt long'''.format(long_read_count, 
+        print('''{} reads were > {} nt long'''.format(long_read_count.value, 
                                                  args.max_length), 
               file = report_file)
 
