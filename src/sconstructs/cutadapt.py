862881f3e280a925f4d995db39f960f062b62fe4
Enrico Gaffo <enrico.gaffo@gmail.com>
2019-10-27 00:51:07 +0200

    fixed parameter names

--- src/sconstructs/cutadapt.py
+++ src/sconstructs/cutadapt.py
@@ -31,15 +31,19 @@ except NameError:
         Exit(1)
 
 env.SetDefault(ADPT_OVERLAP = 5)
+env.SetDefault(MIN_LEN = 15)
 
-cutadapt_cmd = '''cutadapt $( -j $CPUS $) -a $ADAPTER --no-indels -n 10 -O $ADPT_OVERLAP '''\
-               '''--quality-base $QUALITY_ENCODING -m $MIN_LEN -M MAX_LEN --trimmed-only '''\
-               '''--too-long-output ${TARGETS[1]} $CUTADAPT_EXTRA_PARAMS -o ${TARGETS[0]} '''\
-               '''${SOURCES[0]} > ${TARGETS[2]}'''
+cutadapt_cmd = '''cutadapt $( -j $CPUS --quiet $) -a $ADAPTER '''\
+               '''--no-indels -n 10 -O $ADPT_OVERLAP '''\
+               '''--quality-base $QUALITY_ENCODING -m $MIN_LEN -M $MAX_LEN_FILTER '''\
+               '''--trimmed-only '''\
+               '''--too-long-output ${TARGETS[2]} '''\
+               '''$CUTADAPT_EXTRA_PARAMS -o ${TARGETS[0]} '''\
+               '''${SOURCES[0]} > ${TARGETS[1]}'''
 
 cutadapt =  env.Command(['${SAMPLE}_trimmed_reads.fq.gz', 
-                         '${SAMPLE}_long_reads.fq.gz',
-                         '${SAMPLE}_cutadapt.log'], 
+                         '${SAMPLE}_cutadapt.log', 
+                         '${SAMPLE}_long_reads.fq.gz'],
                         env['READS'], 
                         cutadapt_cmd)
 
