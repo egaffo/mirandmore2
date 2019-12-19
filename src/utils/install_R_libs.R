35da458c5b7e3b42d01aa583af36308c88473c3b
Enrico Gaffo <enrico.gaffo@gmail.com>
2019-12-19 17:02:04 +0100

    added optparse R library to install script

--- src/utils/install_R_libs.R
+++ src/utils/install_R_libs.R
@@ -1,6 +1,6 @@
 #!/usr/bin/env Rscript
 
-packs <- c("getopt", "plyr", "data.table")
+packs <- c("getopt", "plyr", "data.table", "optparse")
 
 install.packages(packs, repos="http://cran.r-project.org", dependencies = T)
 
